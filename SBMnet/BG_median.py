978df0d13d985a35fd5d980efa200f04cd628b80
k-nic <59475052+k-nic@users.noreply.github.com>
2022-10-31 23:18:12 +0600

    Add files via upload

--- SBMnet/BG_median.py
+++ SBMnet/BG_median.py
@@ -63,23 +63,41 @@ def BG_median(video_path):
     return Y
 
 def gaussian_kernel(X):
+    dim = X.shape[1]
     Gram = np.matmul(X, np.transpose(X))/dim
-    col = np.reshape(np.sum(X*X,axis=1)/dim, (-1,1))*5
+    col = np.reshape(np.sum(X*X,axis=1)/dim, (-1,1))
     row = np.transpose(col)
-    return np.exp(10*Gram-col-row)
+    return np.exp(2*Gram-col-row)
 
 def variance_kernel(X):
+    dim = X.shape[1]
     Gram = np.matmul(X, np.transpose(X))/dim
     return 1+Gram
 
 def skewness_kernel(X):
+    dim = X.shape[1]
     Gram = np.matmul(X, np.transpose(X))/dim
     return 1+Gram+Gram*Gram
 
 def kurtosis_kernel(X):
+    dim = X.shape[1]
     Gram = np.matmul(X, np.transpose(X))/dim
     GG=Gram*Gram
     return 1+Gram+GG+GG*Gram
+
+def laplace_kernel(X):
+    dim = X.shape[1]
+    Gram = np.matmul(X, np.transpose(X))/dim
+    col = np.reshape(np.sum(X*X,axis=1)/dim, (-1,1))
+    row = np.transpose(col)
+    return np.exp(-5*np.sqrt(col+row-2.0*Gram+0.0001))
+
+def poisson_kernel(X):
+    dim = X.shape[1]
+    Gram = np.matmul(X, np.transpose(X))/dim
+    col = np.reshape(np.sum(X*X,axis=1)/dim, (-1,1))
+    row = np.transpose(col)
+    return np.power(1.0+0.04*(col+row-2.0*Gram), -(dim+1)/2)
 
 def kernel_calc(X, kernel_function, n_components):
     N = X.shape[0]
@@ -196,9 +214,63 @@ def gaussian_median(video_path):
 
 
     X = np.reshape(Frames, (-1,dim))
-    Y = kernel_calc(X,gaussian_kernel,n_components=5)
+    Y = kernel_calc(X,gaussian_kernel,n_components=1)
     Y = np.reshape(Y,Frames.shape[0:4])
     Y = np.median(Y, axis=0)
     Y = np.around(Y)
     Y = Y.astype('uint8')
     return Y
+
+def laplace_median(video_path):
+    Frames = [None] * 10000
+    nFrames = 0
+
+    for dirpath, dirnames, filenames in os.walk(video_path):
+        for filename in [f for f in filenames if f.endswith(".jpg")]:
+            image = cv2.imread(os.path.join(dirpath, filename),-1)
+            x = re.findall(r"in[0]+([1-9][0-9]*)\.jpg", filename)
+            if len(x)==0:
+                Frames[0]=image
+                continue
+            Frames[int(x[0])]=image
+            nFrames = nFrames if (nFrames>int(x[0])) else int(x[0])
+
+    Frames = np.array(Frames[0:nFrames])
+    dim = Frames.shape[1]*Frames.shape[2]*Frames.shape[3]
+
+
+    X = np.reshape(Frames, (-1,dim))
+    Y = kernel_calc(X,laplace_kernel,n_components=5)
+    Y = np.reshape(Y,Frames.shape[0:4])
+    Y = np.median(Y, axis=0)
+    Y = np.around(Y)
+    Y = Y.astype('uint8')
+    return Y
+
+
+def poisson_median(video_path):
+    Frames = [None] * 10000
+    nFrames = 0
+
+    for dirpath, dirnames, filenames in os.walk(video_path):
+        for filename in [f for f in filenames if f.endswith(".jpg")]:
+            image = cv2.imread(os.path.join(dirpath, filename),-1)
+            x = re.findall(r"in[0]+([1-9][0-9]*)\.jpg", filename)
+            if len(x)==0:
+                Frames[0]=image
+                continue
+            Frames[int(x[0])]=image
+            nFrames = nFrames if (nFrames>int(x[0])) else int(x[0])
+
+    Frames = np.array(Frames[0:nFrames])
+    dim = Frames.shape[1]*Frames.shape[2]*Frames.shape[3]
+
+
+    X = np.reshape(Frames, (-1,dim))
+    Y = kernel_calc(X,poisson_kernel,n_components=5)
+    Y = np.reshape(Y,Frames.shape[0:4])
+    Y = np.median(Y, axis=0)
+    Y = np.around(Y)
+    Y = Y.astype('uint8')
+    return Y
+
